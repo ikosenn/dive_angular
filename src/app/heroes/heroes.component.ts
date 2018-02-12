5932bbeb994eff53b9b541298a67c71ce453a02c
ikosenn <i@ikosenn.me>
2018-02-11 23:38:10 -0800

    Create master detail view

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,13 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
+    heroes: Hero[] = HEROES;
     constructor() { }
 
     ngOnInit() {
     }
-
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+	}
 }
